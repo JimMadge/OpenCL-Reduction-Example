ca597ad583e365ae8029f610940190b72decf91f
Jim Madge <jmmadge@gmail.com>
2020-03-26 10:44:04 +0000

    Update padding function for 2D example

--- reduction.py
+++ reduction.py
@@ -3,14 +3,15 @@ import pyopencl as cl
 from pyopencl import mem_flags as mf
 
 
-def pad(array, array_size, group_size):
+def pad(array, group_size):
     """Pad an array with zeros so that it is a multiple of the group size."""
-    remainder = array_size % group_size
+    sum_size = array.shape[1]
+    remainder = sum_size % group_size
     if remainder == 0:
         return array
     else:
-        padding = group_size - array_size % group_size
-        return np.concatenate((array, np.zeros(padding)))
+        padding = group_size - sum_size % group_size
+        return np.hstack((array, np.zeros((array.shape[0], padding))))
 
 
 if __name__ == "__main__":
@@ -31,7 +32,8 @@ if __name__ == "__main__":
 
     # Create and pad an array
     # Notice the initial array is not of dimensions 2^n, or a multiple of 2^n
-    a_h = np.random.random((5, 1024))
+    a_h = np.random.random((3000, 1024*2+32))
+    a_h = pad(a_h, group_size)
     n_rows = a_h.shape[0]
     row_length = a_h.shape[1]
 
@@ -44,8 +46,6 @@ if __name__ == "__main__":
     # Determine memory per work group (total size of array in bytes / number of
     # work groups, or, size of element of array in bytes * of work-group size)
     local_memory_size = a_h[0].nbytes//work_groups_per_row
-    print(work_groups_per_row)
-    print(local_memory_size)
 
     # Create buffers
     a_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a_h)
@@ -53,12 +53,9 @@ if __name__ == "__main__":
     p_d = cl.Buffer(context, mf.WRITE_ONLY, p_h.nbytes)
 
     redsum = program.sum_axis1
-    print(a_h.shape)
-    print(a_h)
     redsum(queue, a_h.shape, (1, group_size,), a_d, b_d, p_d)
 
     cl.enqueue_copy(queue, p_h, p_d)
-    print(p_h)
     print(np.sum(a_h, axis=1))
     print(np.sum(p_h, axis=1))
     assert np.isclose(np.sum(a_h), np.sum(p_h))
