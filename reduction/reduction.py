d28b8bc8b27652a5520ed398fec82d8eeb95cc2e
Jim Madge <jmmadge@gmail.com>
2020-03-26 12:54:29 +0000

    Restore 1D example

--- reduction/reduction.py
+++ reduction/reduction.py
@@ -7,6 +7,51 @@ kernel_file = pathlib.Path(__file__).parent.absolute()/"reduction.cl"
 kernel_source = open(kernel_file).read()
 
 
+def pad(array, group_size):
+    """Pad an array with zeros so that it is a multiple of the group size."""
+    array_size = array.shape[0]
+    remainder = array_size % group_size
+    if remainder == 0:
+        return array
+    else:
+        padding = group_size - array_size % group_size
+        return np.concatenate((array, np.zeros(padding)))
+
+
+def redsum(array, context, program):
+    # Create queue
+    queue = cl.CommandQueue(context)
+
+    # Set the work-group size (number of work-items per work-group)
+    group_size = 256
+
+    # Pad an array
+    a_h = pad(array, group_size)
+
+    # Determine number of work-groups (work-items / work-group size)
+    work_groups = a_h.shape[0]//group_size
+
+    # Assign array of sum per work group
+    p_h = np.zeros(work_groups)
+
+    # Determine memory per work group (total size of array in bytes / number of
+    # work groups, or, size of element of array in bytes * of work-group size)
+    local_memory_size = a_h.nbytes//work_groups
+
+    # Create buffers
+    a_d = cl.Buffer(context, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a_h)
+    b_d = cl.LocalMemory(local_memory_size)
+    p_d = cl.Buffer(context, mf.WRITE_ONLY, p_h.nbytes)
+
+    # Call kernel
+    redsum = program.sum
+    redsum(queue, a_h.shape, (group_size,), a_d, b_d, p_d)
+    cl.enqueue_copy(queue, p_h, p_d)
+
+    # Sum of residuals
+    return np.sum(p_h)
+
+
 def pad_axis1(array, group_size):
     """Pad an array with zeros so that it is a multiple of the group size."""
     sum_size = array.shape[1]
@@ -47,5 +92,5 @@ def redsum_axis1(array, context, program, group_size=256):
     redsum(queue, a_h.shape, (1, group_size,), a_d, b_d, p_d)
     cl.enqueue_copy(queue, p_h, p_d)
 
-    # Sum remaining elements
+    # Sum residuals
     return np.sum(p_h, axis=1)
--- reduction/test/test_reduction.py
+++ reduction/test/test_reduction.py
@@ -1,5 +1,15 @@
 import numpy as np
-from .. import redsum_axis1
+from .. import redsum, redsum_axis1
+
+
+def test_reduction(context, program):
+    # Notice the initial array is not of dimensions 2^n, or a multiple of 2^n
+    array = np.random.random(1024*10 + 3)
+
+    expected_sum = np.sum(array)
+    actual_sum = redsum(array, context, program)
+
+    assert np.allclose(expected_sum, actual_sum)
 
 
 def test_axis1(context, program):
