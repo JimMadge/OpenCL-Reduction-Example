47a32e55c6b8563af6c041961fab45dd06d892e1
Jim Madge <jmmadge@gmail.com>
2020-03-27 13:59:45 +0000

    Generalise padding function

--- reduction/reduction.py
+++ reduction/reduction.py
@@ -7,17 +7,6 @@ kernel_file = pathlib.Path(__file__).parent.absolute()/"reduction.cl"
 kernel_source = open(kernel_file).read()
 
 
-def pad(array, group_size):
-    """Pad an array with zeros so that it is a multiple of the group size."""
-    array_size = array.shape[0]
-    remainder = array_size % group_size
-    if remainder == 0:
-        return array
-    else:
-        padding = group_size - array_size % group_size
-        return np.concatenate((array, np.zeros(padding)))
-
-
 def redsum(array, context, program):
     # Create queue
     queue = cl.CommandQueue(context)
@@ -52,23 +41,12 @@ def redsum(array, context, program):
     return np.sum(p_h)
 
 
-def pad_axis0(array, group_size):
-    """Pad an array with zeros so that it is a multiple of the group size."""
-    sum_size = array.shape[0]
-    remainder = sum_size % group_size
-    if remainder == 0:
-        return array
-    else:
-        padding = group_size - sum_size % group_size
-        return np.vstack((array, np.zeros((padding, array.shape[1]))))
-
-
 def redsum_axis0(array, context, program, group_size=256):
     # Create queue
     queue = cl.CommandQueue(context)
 
     # Pad array
-    a_h = pad_axis0(array, group_size)
+    a_h = pad(array, group_size)
     print(a_h.shape)
     n_cols = a_h.shape[1]
     col_length = a_h.shape[0]
@@ -100,23 +78,12 @@ def redsum_axis0(array, context, program, group_size=256):
     return np.sum(p_h, axis=0)
 
 
-def pad_axis1(array, group_size):
-    """Pad an array with zeros so that it is a multiple of the group size."""
-    sum_size = array.shape[1]
-    remainder = sum_size % group_size
-    if remainder == 0:
-        return array
-    else:
-        padding = group_size - sum_size % group_size
-        return np.hstack((array, np.zeros((array.shape[0], padding))))
-
-
 def redsum_axis1(array, context, program, group_size=256):
     # Create queue
     queue = cl.CommandQueue(context)
 
     # Pad array
-    a_h = pad_axis1(array, group_size)
+    a_h = pad(array, group_size, axis=1)
     n_rows = a_h.shape[0]
     row_length = a_h.shape[1]
 
@@ -142,3 +109,28 @@ def redsum_axis1(array, context, program, group_size=256):
 
     # Sum residuals
     return np.sum(p_h, axis=1)
+
+
+def pad(array, group_size, axis=0):
+    """
+    Pad an array with zeros so that it is a multiple of the group size.
+
+    :arg array: Array to pad.
+    :type array: :class:`numpy.ndarray`
+    :arg int group_size: OpenCL group size.
+    :arg int axis: The axis to pad with zeros. Default is 0.
+
+    :returns: `array` padded with an appropriate number of zeros.
+    :rtype: :class:`numpy.ndarray`
+    """
+    array_size = array.shape[axis]
+    remainder = array_size % group_size
+    if remainder == 0:
+        return array
+    else:
+        padding = group_size - array_size % group_size
+        padding_shape = list(array.shape)
+        padding_shape[axis] = padding
+        return np.concatenate(
+            (array, np.zeros(padding_shape, dtype=array.dtype)), axis=axis
+            )
